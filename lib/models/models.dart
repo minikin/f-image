1b6cd53473c834cac5fd74f87acc7d4b8ee5e621
Sasha Prokhorenko <djminikin@gmail.com>
2019-07-02 11:10:55 +0200

    Add models

---
+++ lib/models/models.dart
@@ -0,0 +1,1 @@
+export 'package:f_image/models/photo.dart';
---
+++ lib/models/photo.dart
@@ -0,0 +1,58 @@
+library photo;
+
+import 'dart:convert';
+
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+import 'package:f_image/models/serializers.dart';
+
+part 'photo.g.dart';
+
+abstract class Photo implements Built<Photo, PhotoBuilder> {
+  @BuiltValueField(wireName: 'id')
+  String get id;
+
+  @BuiltValueField(wireName: 'author')
+  String get author;
+
+  @BuiltValueField(wireName: 'width')
+  int get width;
+
+  @BuiltValueField(wireName: 'height')
+  int get height;
+
+  @BuiltValueField(wireName: 'url')
+  String get url;
+
+  @BuiltValueField(wireName: 'download_url')
+  String get imageUrl;
+
+  Photo._();
+
+  factory Photo([updates(PhotoBuilder b)]) = _$Photo;
+
+  String toJson() {
+    return json.encode(serializers.serializeWith(Photo.serializer, this));
+  }
+
+  static Photo fromJson(String jsonString) {
+    return serializers.deserializeWith(
+        Photo.serializer, json.decode(jsonString));
+  }
+
+  static Serializer<Photo> get serializer => _$photoSerializer;
+
+  static Photo parsePhoto(String responseBody) {
+    return Photo.fromJson(responseBody);
+  }
+
+  static BuiltList<Photo> parseListOfPhotos(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return deserializeListOf<Photo>(parsed);
+  }
+
+  String computedImageUrl() {
+    return 'https://picsum.photos/id/${this.id}/600/600';
+  }
+}
